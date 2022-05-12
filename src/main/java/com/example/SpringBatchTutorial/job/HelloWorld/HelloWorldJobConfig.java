3a4a1e85ad17e87acb56e5037bf1cdfe31920987
MyeongJun Go <myeongjun.ko@gmail.com>
2022-05-12 10:02:52 +0900

    created HelloWorld Job

---
+++ src/main/java/com/example/SpringBatchTutorial/job/HelloWorld/HelloWorldJobConfig.java
@@ -0,0 +1,60 @@
+package com.example.SpringBatchTutorial.job.HelloWorld;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * desc: tasklet을 활용하여 Hello World를 출력
+ * run: --job.name=helloWorldJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer())
+                .start(helloWorldStep())
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step helloWorldStep() {
+        return stepBuilderFactory.get("helloWorldStep")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello World Spring Batch");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
