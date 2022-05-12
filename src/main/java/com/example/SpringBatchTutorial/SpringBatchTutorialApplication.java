b13db317dc5324c6e89905014c3e1e21c5062464
MyeongJun Go <myeongjun.ko@gmail.com>
2022-05-12 09:24:49 +0900

    first commit

---
+++ src/main/java/com/example/SpringBatchTutorial/SpringBatchTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBatchTutorial/SpringBatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
