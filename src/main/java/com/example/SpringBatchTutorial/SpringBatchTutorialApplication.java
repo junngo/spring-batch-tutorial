61b3eb1306712a680e313f8ffa8bc166bedb0890
MyeongJun Go <myeongjun.ko@gmail.com>
2022-05-20 17:52:08 +0900

    added scheduler example

--- src/main/java/com/example/SpringBatchTutorial/SpringBatchTutorialApplication.java
+++ src/main/java/com/example/SpringBatchTutorial/SpringBatchTutorialApplication.java
@@ -3,7 +3,9 @@ package com.example.SpringBatchTutorial;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchTutorialApplication {
---
+++ src/main/java/com/example/SpringBatchTutorial/core/scheduler/SampleScheduler.java
@@ -0,0 +1,35 @@
+package com.example.SpringBatchTutorial.core.scheduler;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class SampleScheduler {
+
+    @Autowired
+    private Job fileReadWriteJob;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Scheduled(cron = "0 */1 * * * *")
+    public void helloWorldJobRun() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        JobParameters jobParameters = new JobParameters(
+                Collections.singletonMap("requestTime", new JobParameter(System.currentTimeMillis()))
+        );
+
+        jobLauncher.run(fileReadWriteJob, jobParameters);
+    }
+}
